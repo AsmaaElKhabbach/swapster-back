caebb44a0ed25fc8ffdd4ad2482b0f3825d075f1
AsmaaElKhabbach <a.elk.ocl@gmail.com>
2023-04-05 15:13:54 +0200

    jwt ok

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -1,100 +1,67 @@
 const dataMapper = require('../dataMapper');
 const emailValidator = require('email-validator');
 const bcrypt = require('bcrypt');
-// const jwt = require
+const jwt = require('jsonwebtoken');
 
+
+function generateAccessToken(email) {
+  return jwt.sign(email, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
 
 const userController = {
 
-    //signup
+  //signup
 
-    register: async (req, res) => {
-        try {
-            const { name, email, password, passwordConfirm } = req.body;
+  signup: async (req, res) => {
+    try {
+      const { name, email, city, password, passwordConfirm } = req.body;
 
-            if (!name) {
-                res.status(500).json({error:"manque name"});
-                console.log(user.name);
-                return;
-            };
+      if (!name) {
+        res.status(500).json({ error: "manque name" });
+        return;
+      };
 
-            if (!emailValidator.validate(email)) {
-                res.status(500).json({error:"email invalide"});
-                console.log(email);
-                return;
-            };
+      if (!city) {
+        res.status(500).json({ error: "manque city" });
+        return;
+      };
 
-            const checkUser = await dataMapper.getOneUserByEmail(email);
+      if (!emailValidator.validate(email)) {
+        res.status(500).json({ error: "email invalide" });
+        return;
+      };
 
-            if (checkUser) {
-                res.status(500).json({error:"email déjà utilisé"});
-                return;
-            };
+      const checkUser = await dataMapper.getOneUserByEmail(email);
 
-            if (password !== passwordConfirm) {
-                res.status(500).json({error:"Le mot de passe ne correspond pas."});
-                return;
-            };
+      if (checkUser) {
+        res.status(500).json({ error: "email déjà utilisé" });
+        return;
+      };
 
-            password = await bcrypt.hash(password, 10);
+      if (password !== passwordConfirm) {
+        res.status(500).json({ error: "Le mot de passe ne correspond pas." });
+        return;
+      };
 
-            const userData = await dataMapper.insertUser(user);
-            console.log(userData);
-            res.status(201).json(userData);
+      const user = {
+        name: name,
+        email: email,
+        city: city,
+        password: await bcrypt.hash(password, 10)
+      };
 
-          } catch(err) {
-            res.status(500).json({error:"Erreur lors de l'insertion de l'utilisateur dans la base de données"});
-          }
-        },
-    
-    // Methode pour se logger
+      const token = generateAccessToken({ email: req.body.email });
 
-    login: async (req, res) => {
-        // On récupère l'email et le password dans le body
-        const { email, password } = req.body;
+      const userData = await dataMapper.insertUser(user);
+      res.status(201).json(userData, token);
 
-        try {
-            // On stock le resultat de la requête
-            const user = await dataMapper.getOneUserByEmail(email);
-
-            // On vérifie que l'email correspond 
-
-            if (!user) {
-              return res.status(401).json({ error: "Email ou mot de passe incorrect" });
-            }
-            // On vérifie que le password correspond avec bcrypt
-            //  const passwordIsGood = await bcrypt.compare(password, user.password);
-
-            //  if (!passwordIsGood) {
-            //    res.status(500).send("Email ou mot de passe incorrect")
-
-            //  }
-
-            if (user.password !== password) {
-              return res.status(401).json({ error: "Email ou mot de passe incorrect" })
-
-            }
-            // On crée un token et on le renvoie au client 
-            // const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
-            // res.json({ token });
-
-
-            res.status(201).json(user)
-
-    } catch (error) {
-      console.error(error)
-      res.status(500).send("Erreur serveur")
-
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Erreur lors de l'insertion de l'utilisateur dans la base de données" });
     }
-  },
-  // Logout Method
+  }
 
-  // logout: async (req, res) => {
-  //   // On supprime le token du local storage
-  //   localStorage.removeItem('token');
-  //   res.status(200).json({ message: "Vous êtes bien déconnecté" })
 
-  // }
 
 }
 
--- app/router/index.js
+++ app/router/index.js
@@ -1,13 +1,17 @@
 // const { Router } = require('express')
 const express = require('express');
 const userController = require('../controller/userController');
+const sessionController = require('../controller/sessionController');
+const authenticateToken = require('../middelware/jwt')
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Youpi');
 });
+router.post('/signup', authenticateToken, userController.signup)
 
-router.post('/login', userController.login)
-router.post('/logout', userController.logout)
+router.post('/login', sessionController.login)
+
+// router.post('/logout', userController.logout)
 
 module.exports = router;
