7acda6e6833b37c9eaf106cd46dcb33e47e71680
AsmaaElKhabbach <a.elk.ocl@gmail.com>
2023-04-06 10:29:42 +0200

    add cors and modification route login

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const router = require('./app/router');
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(router);
 
