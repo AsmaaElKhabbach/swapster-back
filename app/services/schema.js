2a1e882c348235c36f11fced17edc8e492c92868
mjkim <kaitar0.0w0@gmail.com>
2023-04-12 15:34:33 +0200

    correction merge schema.js

--- app/services/schema.js
+++ app/services/schema.js
@@ -1,6 +1,5 @@
 const Joi = require('joi')
 
-<<<<<<< HEAD
 // on crée un schéma qui va valider le signup
 const signup = Joi.object({
 	name: Joi.string().alphanum().min(4).required(),
@@ -35,41 +34,8 @@ const searchBook = Joi.object({
 
 // on crée un schéma qui va valider l'id du book
 const bookId = Joi.number().required();
-=======
-// Je crée un schéma qui va valider le signup
-
-const signup = Joi.object({
-  name: Joi.string().alphanum().min(4).required(),
-  email: Joi.string().email().required(),
-  city: Joi.string().pattern(/^[a-zA-Z]{4,50}$/).required(),
-  password: Joi.string().pattern(/^[a-zA-Z0-9]{4,}$/).required(),
-  passwordConfirm: Joi.ref("password")
-})
-
-
-const login = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required()
-})
-
-const userId = Joi.number().required();
-
-const updateUser = Joi.object({
-  name: Joi.string().alphanum().min(4),
-  email: Joi.string().email(),
-  city: Joi.string().pattern(/^[a-zA-Z]{4,50}$/),
-  password: Joi.string().pattern(/^[a-zA-Z0-9]{4,}$/),
-  passwordConfirm: Joi.ref("password")
-})
-
-
->>>>>>> origin/main
 
 
 
 
-<<<<<<< HEAD
 module.exports = { signup, login, userId, updateUser, searchBook,  bookId };
-=======
-module.exports = { signup, login, userId, updateUser };
->>>>>>> origin/main
