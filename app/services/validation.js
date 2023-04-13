22dbe3ddc87caae8a804a18a5aa2a723e61bf719
AsmaaElKhabbach <a.elk.ocl@gmail.com>
2023-04-13 14:30:47 +0200

    corrected validation

--- app/services/validation.js
+++ app/services/validation.js
@@ -34,7 +34,7 @@ const validation = {
 	},
 
 	searchBook(req, res, next) {
-		const { error } = schema.searchBook.validate(req.body, { abortEarly: false })
+		const { error } = schema.searchBook.validate(req.query, { abortEarly: false })
 		if (error) {
 			return res.status(400).json({ error: error.details });
 		}
