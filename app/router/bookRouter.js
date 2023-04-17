6713825c07864693a40d2f12c7b03e57de129d85
AsmaaElKhabbach <a.elk.ocl@gmail.com>
2023-04-17 11:37:35 +0200

    corrected router book

--- app/router/bookRouter.js
+++ app/router/bookRouter.js
@@ -7,17 +7,17 @@ const bookRouter = express.Router();
 
 
 // route des livres de l'utilisateur
-bookRouter.get('/book/my', validateToken, validation.userId, userHasBookController.userBooks);
-bookRouter.post('/book/:bookId/my', validateToken, validation.bookId, validation.userId, validation.addUserBook, userHasBookController.addUserBook);
-bookRouter.patch('/book/:bookId/my', validateToken, validation.updateUserBook, validation.bookId, validation.userId, userHasBookController.updatedUserBook);
-bookRouter.delete('/book/:bookId/my', validateToken, validation.userId, validation.bookId, userHasBookController.deleteUserBook);
-bookRouter.get('/book/givenbook/my', validateToken, validation.userId, userHasBookController.givenUserBook);
+bookRouter.get('/my', validateToken, validation.userId, userHasBookController.userBooks);
+bookRouter.post('/:bookId/my', validateToken, validation.bookId, validation.userId, validation.addUserBook, userHasBookController.addUserBook);
+bookRouter.patch('/:bookId/my', validateToken, validation.updateUserBook, validation.bookId, validation.userId, userHasBookController.updatedUserBook);
+bookRouter.delete('/:bookId/my', validateToken, validation.userId, validation.bookId, userHasBookController.deleteUserBook);
+bookRouter.get('/givenbook/my', validateToken, validation.userId, userHasBookController.givenUserBook);
 
 // route livre(s)
-bookRouter.get('/book/latestadded', bookController.latestbooks);
-bookRouter.get('/book/search', validation.searchBook, bookController.searchBook)
-bookRouter.get('/book/:bookId/allusers', validation.bookId, bookController.allUsersByBookId);
-bookRouter.get('/book/:bookId', validation.bookId, bookController.bookDetails);
+bookRouter.get('/latestadded', bookController.latestbooks);
+bookRouter.get('/search', validation.searchBook, bookController.searchBook)
+bookRouter.get('/:bookId/allusers', validation.bookId, bookController.allUsersByBookId);
+bookRouter.get('/:bookId', validation.bookId, bookController.bookDetails);
 
 
 
