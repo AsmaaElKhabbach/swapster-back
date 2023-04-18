16eac4837382ad8e3e16426c95315324d4d27957
AsmaaElKhabbach <a.elk.ocl@gmail.com>
2023-04-18 14:48:21 +0200

    add swagger documentation for user's path

--- app/router/index.js
+++ app/router/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const userRouter = require('./userRouter');
 const bookRouter = require('./bookRouter');
 const router = express.Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../utils/swagger.json');
+
+
 
 router.get('/', (req, res) => {
 	res.send('Youpi');
 });
+
 
 
 // route inscription, login, logout, modification et suppression de l'utilisateur
@@ -17,7 +18,5 @@ router.use('/user', userRouter);
 router.use('/book', bookRouter);
 
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
