8a20ce0150932a1e12f7559df56a874e80bf3803
mjkim <kaitar0.0w0@gmail.com>
2023-04-20 10:49:22 +0200

    comment APIError

--- app/utils/error/APIError.js
+++ app/utils/error/APIError.js
@@ -1,8 +1,10 @@
+// APIError enfant de Error
+
 class APIError extends Error {
     constructor(code,msg){
         super(msg);
         this.status = code;
     }
-}
+};
 
 module.exports = APIError;
